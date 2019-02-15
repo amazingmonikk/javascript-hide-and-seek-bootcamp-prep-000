5ccad2e96bb3ffd3b523266544fe86e661f420d6
Monika Tadross <monikk.tadross@gmail.com>
2019-02-15 20:37:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function increaseRankBy(n) {
 function deepestChild() {
   //var list = document.querySelectorAll('#grand-node div')
  
- let current = "#grand-node"
+ let current = document.querySelectorAll("#grand-node")
   let next = []
   
    while (current) {
