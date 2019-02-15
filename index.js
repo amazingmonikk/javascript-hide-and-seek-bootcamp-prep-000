fb307483fe9d5aff87ea024aa53a3117253dcfe2
Monika Tadross <monikk.tadross@gmail.com>
2019-02-15 19:08:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+function getFirstSelector(selector) {
+  var element = document.querySelector(selector);
 
+return element;
+}
+
+function nestedTarget() {
+  const target = document.querySelector('#nested .target');
+  return target;
+
+}
+
+
+function increaseRankBy(n) {
+  
+  var result = document.querySelectorAll('.ranked-list')
+  console.log(result)
+  for (var i = 0; i < result.length; i++) {
+   parseInt(result) + n
+ 
+   
+}
+}
+
+
+
+
+function deepestChild() {
+  var list = document.querySelectorAll('#grand-node div')
+ 
+  while ('grand-node div' has children) {
+  overwrite current node with the first child;
+}
+  return the current after the loop
+ 
+}
