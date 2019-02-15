5fcb5ba2012ccb73049049300f30275cf321fbea
Monika Tadross <monikk.tadross@gmail.com>
2019-02-15 21:09:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+function getFirstSelector(selector) {
+  var element = document.querySelector(selector);
 
+return element;
+}
+
+function nestedTarget() {
+  const target = document.querySelector('#nested .target');
+  return target;
+
+}
+
+
+function increaseRankBy(n) {
+  
+  var result = document.querySelectorAll('.ranked-list li')
+  console.log(result)
+  for (var i = 0; i < result.length; i++) {
+  var newNumber = parseInt(result[i].innerHTML) + n
+  result[i].innerHTML = newNumber;
+   
+}
+}
+
+
+
+
+function deepestChild() {
+  
+ 
+ let current = document.querySelector("#grand-node")
+  let child = current.children[0]
+  
+   while (child) {
+      current = child
+      child = current.children[0]
+   }
+      
+    return current;
+    
+} 
+ 
+ 
+
+ 
+ 
