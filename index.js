7403186754bb4e2f05f7e286f350cad23e9d0d61
Monika Tadross <monikk.tadross@gmail.com>
2019-02-15 20:51:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,19 +36,11 @@ function deepestChild() {
       child = current.children[0]
    }
       
+    return child
     
-    
-     if (Array.isArray(current)) {
-      for (let i = 0; i < current.length; i++) {
-        next.push(current[i])
-      }
-    }
-    
-    current = next.shift()
-  }
+} 
  
  
-  return null
-}
+
  
  
