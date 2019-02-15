9dc9da7c945e63ccbd8a3a045aac00bca78c1ad6
Monika Tadross <monikk.tadross@gmail.com>
2019-02-15 20:55:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   
-  var result = document.querySelectorAll('.ranked-list')
+  var result = document.querySelectorAll('.ranked-list li')
   console.log(result)
   for (var i = 0; i < result.length; i++) {
    parseInt(result) + n
